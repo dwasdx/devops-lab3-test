de6c96bd6233423ce07256ff561e1e4435653793
Andrei Zhuravlev <an.zhuravlev@globus-ltd.com>
2020-06-03 09:29:59 +0300

    file changed

--- phonebook.py
+++ phonebook.py
@@ -3,12 +3,8 @@ from phonebook_functional import *
 
 comm = 1
 while comm:
-    print('Choose one of the commands',
-          'add - Add new record to the phonebook',
-          'delete - Delete existing record to the phonebook',
-          'change - Change some field in existing record',
-          'print all - Print all phonebook',
-          'print age - Print a certain person\'s age',
+    print('Choose one of the commands', 'add - Add new record to the phonebook', 'delete - Delete existing record to the phonebook', 'change - Change some field in existing record',
+          'print all - Print all phonebook','print age - Print a certain person\'s age',
           'search - Search for something in whole phonebook',
           '0 - Exit',
           sep = '\n')
